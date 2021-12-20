a8a6c6c61194c49e4b6440beb6426b4571ba6708
JosiahAlanLee <45522907+JosiahAlanLee@users.noreply.github.com>
2021-12-20 13:50:52 -0600

    Added helper functions
    - known issue : fxhash variables are not currently working, neither are the helper functions

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-//_hydra_overlay_002
+new p5();
+
+function rnd_btw(min, max) {return fxrand() * (max - min) + min;}
+function rnd_btwexp(min, max) {return fxrand()**2 * (max - min) + min;}
+function rnd_int(min, max) {min = Math.ceil(min);max = Math.floor(max);return Math.floor(fxrand() * (max - min + 1)) + min;}
+
+//random function helpers
+// fxrand() gives u a value between 0 and 1
+// rnd_btw(a,b) gives u a value between a and b
+// rnd_btwexp(a,b) gives u a value between a and b, but with an exponential slope (more probable to get the borders than the center)
+// rnd_int(a,b) gives u an integer value between a and b
 
 /* CUSTOM FUNCTIONS FOR P5LIVE */
 // keep fullscreen if window resized
